aaf0821870dbf86d547233c7e694f2a9a54e0bd4
ievas21 <ievas@bu.edu>
2025-03-17 21:38:06 -0400

    Added the API endpoints in main.py (sign-up and login) without authentication

--- fastapi-backend/app/crud.py
+++ fastapi-backend/app/crud.py
@@ -2,7 +2,9 @@ from sqlalchemy.orm import Session
 from app import models, schemas
 
 # create user object using request data after taking a database session
-def create_user(db: Session, user: schemas.UserCreate):
+
+
+def create_user(db: Session, user: schemas.CreateUser):
     db_user = models.User(email=user.email, password=user.password)
     # add this new user to the user table
     db.add(db_user)
@@ -13,5 +15,9 @@ def create_user(db: Session, user: schemas.UserCreate):
     return db_user
 
 # queries the user table using the ORM by email
-def get_email(db: Session, email: str):
-    return db.query(models.User).filter(models.User.email == email)
+
+def get_email(db: Session, email: str, password: str):
+    return db.query(models.User).filter(
+        models.User.email == email,
+        models.User.password == password
+    ).first()
--- fastapi-backend/app/main.py
+++ fastapi-backend/app/main.py
@@ -1,1 +1,60 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.database import get_db, Base, engine
+from app.models import User
+from app.schemas import CreateUser, UserResponse
+from app.crud import create_user, get_email
 
+# file that defines the API endpoints to login and sign-up a user
+
+# intialize thr fastapi app
+app = FastAPI()
+
+# create database tables on startup -- only for in development
+Base.metadata.create_all(bind=engine)
+
+# api endpoint to sign-up a user, responding with the user response defined in schemas.py
+
+
+@app.post("/signup", response_model=UserResponse)
+# need to interact with database (Depends(get_db))
+def signup(user: CreateUser, db: Session = Depends(get_db)):
+    # require the user to provide an email and password
+    if not user.email or not user.password:
+        raise HTTPException(
+            status_code=400, detail="Email and password are required.")
+    # check if this email is already in use
+    existing_user = get_email(db, user.email, user.password)
+    if existing_user:
+        raise HTTPException(
+            status_code=400, detail="Email already registered in Spark! Bytes.")
+
+    else:
+        # create a new user with these credentials in this db
+        new_user = create_user(db, user)
+        return new_user
+
+
+# api endpoint to login an existing user
+@app.post("/login")
+# need to interact with database (Depends(get_db))
+def login(user: CreateUser, db: Session = Depends(get_db)):
+    if not user.email or not user.password:
+        raise HTTPException(
+            status_code=400, detail="Email and password are required.")
+
+    existing_user = get_email(db, user.email, user.password)
+    # check if the user already exists or not
+    if not existing_user:
+        raise HTTPException(
+            status_code=400, detail="Incorrect credentials. Please try again.")
+
+    else:
+        return {"message": "Login successful"}
+
+# returns all users found in the user database
+@app.get("/users")
+def get_all_users(db: Session = Depends(get_db)):
+    # ORM query for all users
+    users = db.query(User).all()
+    return users
