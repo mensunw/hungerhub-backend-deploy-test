933e54b3f0995162747dd5701b11758e24da083b
ievas21 <ievas@bu.edu>
2025-03-17 20:16:13 -0400

    Added database.py for creating database sessions

---
+++ fastapi-backend/app/database.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+## get the database url from the .env file
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# SQLAlchemy manages connections to our database (communicating with PostgreSQL)
+engine = create_engine(DATABASE_URL)
+
+# creates database sessions for SQLAlchemy
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# base class for defining database models (all database tabels with inherit from this class to map python classes to tables)
+Base = declarative_base()
+
+
+# create a new session, and allow for injection in FastAPI
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
